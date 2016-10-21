ffa857d1927c4b6b91fa312ed3f73cb74597bd18
Vladimir Ananko <nograsby@gmail.com>
2016-10-21 11:43:34 +0300

    [TEST-2] DataTypes

---
+++ 6.DataTypes/6.DataTypes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  6.DataTypes
+//
+//  Created by Nograsby on 4/10/16.
+//  Copyright © 2016 AV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
