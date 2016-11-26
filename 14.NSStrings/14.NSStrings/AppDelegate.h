c1b4a3386859df092ab1265794b6fd7dbb783b64
Vladimir Ananko <nograsby@gmail.com>
2016-11-26 14:00:43 +0300

    [TEST-9] Strings

---
+++ 14.NSStrings/14.NSStrings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  14.NSStrings
+//
+//  Created by Vladimir Ananko on 11/9/16.
+//  Copyright © 2016 Vladimir Ananko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 14.NSStrings/14.NSStrings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  14.NSStrings
+//
+//  Created by Vladimir Ananko on 11/9/16.
+//  Copyright © 2016 Vladimir Ananko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
