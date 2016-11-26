fb2790467fbe4d5767f5da4c9c624a3945c25e71
Vladimir Ananko <nograsby@gmail.com>
2016-11-26 20:14:00 +0300

    [TEST-10] BitwiseOperations

---
+++ 15.BitwiseOperations/15.BitwiseOperations/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  15.BitwiseOperations
+//
+//  Created by Vladimir Ananko on 11/25/16.
+//  Copyright © 2016 Vladimir Ananko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ 15.BitwiseOperations/15.BitwiseOperations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  15.BitwiseOperations
+//
+//  Created by Vladimir Ananko on 11/25/16.
+//  Copyright © 2016 Vladimir Ananko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
