ae9340ce5a45671de7ba70896fff2c23f22e22c9
Vladimir Ananko <nograsby@gmail.com>
2016-12-04 22:04:21 +0300

    [TEST-14] ViewAnimations

---
+++ 21.ViewAnimations/21.ViewAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  21.ViewAnimations
+//
+//  Created by Vladimir Ananko on 12/3/16.
+//  Copyright © 2016 Vladimir Ananko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
